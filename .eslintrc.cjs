825df4e26e816e6ec4859a0d6475af7d2f25468d
shihongxins <shihongxins@163.com>
2024-01-31 10:48:07 +0800

    feat: 初始化仓库

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+};
