14f8e4b5a0b7a4559f9c7162571e0cf73a8c8443
shihongxins <shihongxins@163.com>
2024-02-01 18:00:47 +0800

    build(.env): 添加 .env 文件配置项目环境

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,34 @@
 import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 import UnoCSS from 'unocss/vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), eslintPlugin(), UnoCSS()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log('import.meta', import.meta, env);
+  return {
+    base: env.VITE_BASE,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {},
+    plugins: [vue(), eslintPlugin(), UnoCSS()],
+    css: {
+      preprocessorOptions: {
+        sass: {},
+      },
+      postcss: 'postcss.config.cjs',
     },
-    postcss: 'postcss.config.cjs',
-  },
+    server: {
+      proxy: {
+        [env.VITE_API_BASE_PATH]: {
+          target: env.VITE_ORIGIN,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
